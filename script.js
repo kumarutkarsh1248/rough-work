9119fb8806de2b58737a3f168cb47c1aec614207
kumarutkarsh1248 <kumar.utkarsh.civ22@iitbhu.ac.in>
2023-05-28 14:43:17 +0530

    at this point of time i have added all the files

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function print(){
+    console.log("hello");
+}
+print();
+//listening to the keys
+window.addEventListener("keydown", e=>{
+    console.log(e);
+
+    switch (e.key) {
+        
+        case "ArrowUp":
+            print();
+            break;
+        case "ArrowDown":
+            print();
+            break;
+        case "ArrowLeft":
+            print();
+            break;
+        case "ArrowRight":
+            print();
+            break;
+        default:
+            break;
+    }
+}); 
